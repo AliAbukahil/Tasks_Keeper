fdc81cafe143c173f9209e21ecaaa6abd22f8fb9
ali.abukahil <ali.abukahil@gmail.com>
2022-10-18 19:32:49 +0200

    pass object as props

--- airBnb/src/App.js
+++ airBnb/src/App.js
@@ -2,63 +2,18 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
-
-import airBnbData from "./data";
-
-/*
-Challenge:
-
-- import the array of data from data.js
-- map over the array to create <Card /> components
-- display the array of card components under the navbar
-  (in place of the current <Card /> component)
-
-Note: We haven't styled the group of components yet, so they'll
-still be block elements, stacked vertically. We'll add styling later.
-*/
+import data from "./data";
 
 export default function App() {
-  // <Hero />
   return (
     <div>
       <Navbar />
-
-      {airBnbData.map(
-        ({
-          id,
-          title,
-          description,
-          price,
-          coverImg,
-          stats: rating,
-          stats: reviewCount,
-          location,
-          openSpots,
-        }) => {
-          return (
-            <Card
-              key={id}
-              title={title}
-              description={description}
-              price={price}
-              coverImg={coverImg}
-              rating={rating}
-              reviewCount={reviewCount}
-              location={location}
-              openSpots={openSpots}
-            />
-          );
-        }
-      )}
-
-      {/*       <Card
-        img="katie-zaferes.png"
-        rating="5.0"
-        reviewCount={6}
-        location="USA"
-        title="Life Lessons with Katie Zaferes"
-        price={136}
-      /> */}
+      <Hero />
+      <section className="cards-list">
+        {data.map((item) => (
+          <Card key={item.id} item={item} />
+        ))}
+      </section>
     </div>
   );
 }
--- airBnb/src/components/Card.js
+++ airBnb/src/components/Card.js
@@ -1,28 +1,30 @@
 import React from "react";
 
-export default function Card({
-  id,
-  title,
-  description,
-  price,
-  coverImg,
-  stats: rating,
-  stats: reviewCount,
-  location,
-  openSpots,
-}) {
+export default function Card(props) {
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.location === "Online") {
+    badgeText = "ONLINE";
+  }
+
+  /*
+    Challenge: Fix our component! 😱
+    */
+
   return (
     <div className="card">
-      <img src={`../images/${coverImg}`} className="card--image" />
+      {badgeText && <div className="card--badge">{badgeText}</div>}
+      <img src={`../images/${props.item.coverImg}`} className="card--image" />
       <div className="card--stats">
         <img src="../images/star.png" className="card--star" />
-        <span>{rating}</span>
-        <span className="gray">({reviewCount}) • </span>
-        <span className="gray">{location}</span>
+        <span>{props.item.stats.rating}</span>
+        <span className="gray">({props.item.stats.reviewCount}) • </span>
+        <span className="gray">{props.item.location}</span>
       </div>
-      <p>{title}</p>
-      <p>
-        <span className="bold">From ${price}</span> / person
+      <p className="card--title">{props.item.title}</p>
+      <p className="card--price">
+        <span className="bold">From ${props.item.price}</span> / person
       </p>
     </div>
   );
