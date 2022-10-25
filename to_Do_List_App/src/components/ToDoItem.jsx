ab99142d7d0250d4f213e7e70819927bd01b5b66
ali.abukahil <ali.abukahil@gmail.com>
2022-10-25 23:23:42 +0200

    material UI

--- to_Do_List_App/src/components/ToDoItem.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ToDoItems(props) {
-  return (
-    <div onClick={() => props.onChecked(props.id)}>
-      <li>{props.text}</li>
-    </div>
-  );
-}
